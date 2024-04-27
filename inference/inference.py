38fc66e34426b9cf191eb81e7c2e4b163f06e712
zanilia <yinghuo302@163.com>
2024-04-27 11:44:00 +0800

    change tokenizer

--- inference/inference.py
+++ inference/inference.py
@@ -1,43 +1,15 @@
 import numpy as np
 import os
-from sentencepiece import SentencePieceProcessor
 from typing import Any, Generator, List,Tuple
 from threading import Lock
-
-class Tokenizer:
-    def __init__(self, model_path: str):
-        # reload tokenizer
-        assert os.path.isfile(model_path), model_path
-        self.sp_model = SentencePieceProcessor(model_file=model_path)
-
-        # BOS / EOS token IDs
-        self.n_words: int = self.sp_model.vocab_size()
-        self.bos_id: int = self.sp_model.bos_id()
-        self.eos_id: int = self.sp_model.eos_id()
-        self.pad_id: int = self.sp_model.pad_id()
-        self.eos_token_id = self.eos_id
-        assert self.sp_model.vocab_size() == self.sp_model.get_piece_size()
-
-    def encode(self, s: str, bos: bool=True, eos: bool=False) -> List[int]:
-        assert type(s) is str
-        t = self.sp_model.encode(s)
-        if bos:
-            t = [self.bos_id] + t
-        if eos:
-            t = t + [self.eos_id]
-        return t
-
-    def decode(self, t: List[int]) -> str:
-        return self.sp_model.decode(t)
 
 from session import Session
 from config import InferenceConfig
 class LlamaInterface:
     def __init__(self,config:InferenceConfig) -> None:
         self.max_length = config.max_length
-        self.tokenizer=Tokenizer(config.tokenizer)
-        # from transformers import LlamaTokenizer
-        # self.tokenizer:LlamaTokenizer=LlamaTokenizer.from_pretrained(config.tokenizer)
+        from transformers import AutoTokenizer
+        self.tokenizer=AutoTokenizer.from_pretrained(config.tokenizer)
         self.sampling_method=config.sampling_method
         self.sampling_value = config.sampling_value
         self.temperature=config.temperature
